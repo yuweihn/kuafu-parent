a4815eb6e4f130278c50f6435c0f08a9dafeb09f
wei.yu <weiy.yu@os.51job.com>
2024-10-16 14:06:54 +0800

    1.0.1

--- kuafu-core/src/main/java/com/yuweix/kuafu/core/mq/rabbit/DefaultRabbitSender.java
+++ kuafu-core/src/main/java/com/yuweix/kuafu/core/mq/rabbit/DefaultRabbitSender.java
@@ -38,7 +38,7 @@ public class DefaultRabbitSender implements RabbitSender, Confirmable {
             rabbitTemplate.convertAndSend(exchange, routeKey, msg, new ConfirmData(exchange, routeKey, msg, this));
         } catch (Exception e) {
             log.error("发送消息异常, Error: {}", e.getMessage(), e);
-            throw new RuntimeException("发送消息异常");
+            throw new RuntimeException("发送消息异常" + e.getMessage());
         }
     }
 
