107bad1014c2c7f9d558f8f538cdb291b1c837b0
yuwei <yuwei@yuweix.com>
2025-06-14 20:03:39 +0800

    1.0.3

--- kuafu-core/src/main/java/com/yuweix/kuafu/core/mq/rabbit/AbstractRetryRabbitReceiver.java
+++ kuafu-core/src/main/java/com/yuweix/kuafu/core/mq/rabbit/AbstractRetryRabbitReceiver.java
@@ -2,13 +2,12 @@ package com.yuweix.kuafu.core.mq.rabbit;
 
 
 import com.rabbitmq.client.Channel;
+import com.yuweix.kuafu.core.SpringContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.retry.support.RetryTemplate;
-
-import javax.annotation.Resource;
 
 
 /**
@@ -18,18 +17,34 @@ import javax.annotation.Resource;
 public abstract class AbstractRetryRabbitReceiver<T> extends AbstractBaseRabbitReceiver<T> {
     private static final Logger log = LoggerFactory.getLogger(AbstractRetryRabbitReceiver.class);
 
-    @Resource
-    protected RetryTemplate retryTemplate;
+    private RetryTemplate retryTemplate;
+    private static final Object LOCK = new Object();
 
 
     @RabbitHandler(isDefault = true)
     public void onMessage(Message message, Channel channel) {
-        retryTemplate.execute(retryCallback -> {
+        getRetryTemplate().execute(retryCallback -> {
             super.handleMessage(message, channel);
             return null;
         }, recoveryCallback -> {
             channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
             return null;
         });
+    }
+
+    public void setRetryTemplate(RetryTemplate retryTemplate) {
+        this.retryTemplate = retryTemplate;
+    }
+
+    protected RetryTemplate getRetryTemplate() {
+        if (retryTemplate != null) {
+            return retryTemplate;
+        }
+        synchronized (LOCK) {
+            if (retryTemplate == null) {
+                retryTemplate = SpringContext.getBean(RetryTemplate.class);
+            }
+        }
+        return retryTemplate;
     }
 }
