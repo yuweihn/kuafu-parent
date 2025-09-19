4bddbcad29901c2e8f3fce95ce0f1e11c43eae7b
wei.yu <weiy.yu@os.51job.com>
2025-09-19 15:55:24 +0800

    1.0.4

--- kuafu-core/src/main/java/com/yuweix/kuafu/core/ActionUtil.java
+++ kuafu-core/src/main/java/com/yuweix/kuafu/core/ActionUtil.java
@@ -15,8 +15,8 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -202,20 +202,20 @@ public abstract class ActionUtil {
 		return (String) request.getServletContext().getAttribute(Constant.STATIC_PATH_KEY);
 	}
 
-	public static void output(String str) {
-		output(str.getBytes(StandardCharsets.UTF_8), "text/html");
-	}
+    public static void output(byte[] bytes) {
+        output(bytes, "text/html");
+    }
 
-	public static void output(byte[] content, String contentType) {
-		output(content, contentType, null);
+	public static void output(byte[] bytes, String contentType) {
+		output(bytes, contentType, null);
 	}
 
 	/**
 	 * contentType 不含字符集
 	 */
-	public static void output(byte[] content, String contentType, Map<String, String> headers) {
-        if (content == null) {
-            log.warn("content为空");
+	public static void output(byte[] bytes, String contentType, Map<String, String> headers) {
+        if (bytes == null) {
+            log.warn("响应内容为空");
             return;
         }
 		HttpServletResponse resp = getResponse();
@@ -235,7 +235,7 @@ public abstract class ActionUtil {
 		}
 
 		try {
-            bis = new ByteArrayInputStream(content);
+            bis = new ByteArrayInputStream(bytes);
             bfis = new BufferedInputStream(bis);
             OutputStream os = resp.getOutputStream();
             byte[] buffer = new byte[1024];
@@ -261,6 +261,21 @@ public abstract class ActionUtil {
             }
         }
 	}
+
+    public static void download(byte[] bytes, String fileName) {
+        try {
+            String attachFileName = URLEncoder.encode(fileName, Constant.ENCODING_UTF_8);
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Content-Disposition", "attachment;filename=" + attachFileName);
+            headers.put("_filename", attachFileName);
+            headers.put("Access-Control-Expose-Headers", "_filename");
+            output(bytes, "application/octet-stream", headers);
+        } catch (Exception e) {
+            log.error("下载文件失败，Error: {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
 
 	/**
 	 * 转发到指定URL
