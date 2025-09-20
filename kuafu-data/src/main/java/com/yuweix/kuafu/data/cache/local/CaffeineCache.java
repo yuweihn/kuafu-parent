d01bfc41f61dd719cf6e2d4ead15b407df95e2f6
yuwei <yuwei@yuweix.com>
2025-09-20 15:55:48 +0800

    2.0.3

--- kuafu-data/src/main/java/com/yuweix/kuafu/data/cache/local/CaffeineCache.java
+++ kuafu-data/src/main/java/com/yuweix/kuafu/data/cache/local/CaffeineCache.java
@@ -17,14 +17,14 @@ import java.util.concurrent.TimeUnit;
  * @author yuwei
  */
 public class CaffeineCache implements LocalCache {
-	private static final Logger log = LoggerFactory.getLogger(CaffeineCache.class);
+    private static final Logger log = LoggerFactory.getLogger(CaffeineCache.class);
 
 
-	private LoadingCache<String, String> cache = null;
+    private LoadingCache<String, String> cache = null;
 
 
     public CaffeineCache(long duration) {
-		try {
+        try {
             cache = Caffeine.newBuilder()
                     .expireAfterAccess(duration, TimeUnit.SECONDS)
                     .removalListener(new RemovalListener<String, String>() {
@@ -43,168 +43,177 @@ public class CaffeineCache implements LocalCache {
                             return null;
                         }
                     });
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * @param duration   缓存时间(单位：秒)
-	 */
-	public static CaffeineCache create(long duration) {
-		return new CaffeineCache(duration);
-	}
+    /**
+     * @param duration   缓存时间(单位：秒)
+     */
+    public static CaffeineCache create(long duration) {
+        return new CaffeineCache(duration);
+    }
 
-	public boolean put(String key, String value) {
-		try {
+    @Override
+    public boolean put(String key, String value) {
+        try {
             cache.put(key, value);
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Caffeine设置缓存值出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Caffeine设置缓存值出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	public boolean putAll(Map<String, String> map) {
-		try {
+    @Override
+    public boolean put(Map<String, String> map) {
+        try {
             cache.putAll(map);
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Caffeine批量设置缓存值出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Caffeine批量设置缓存值出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	public String get(String key) {
-		String val = null;
-		try {
-			val = cache.get(key, t -> null);
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-		} catch (Exception e) {
-			log.error("Caffeine获取缓存值出错，Error: {}", e.getMessage(), e);
-		}
-		return val;
-	}
+    @Override
+    public String get(String key) {
+        String val = null;
+        try {
+            val = cache.get(key, t -> null);
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+        } catch (Exception e) {
+            log.error("Caffeine获取缓存值出错，Error: {}", e.getMessage(), e);
+        }
+        return val;
+    }
 
-	public String getIfPresent(String key) {
-		String val = null;
-		try {
-			val = cache.getIfPresent(key);
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-		} catch (Exception e) {
-			log.error("Caffeine获取缓存值出错，Error: {}", e.getMessage(), e);
-		}
-		return val;
-	}
+    @Override
+    public String getIfPresent(String key) {
+        String val = null;
+        try {
+            val = cache.getIfPresent(key);
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+        } catch (Exception e) {
+            log.error("Caffeine获取缓存值出错，Error: {}", e.getMessage(), e);
+        }
+        return val;
+    }
 
-	public boolean remove(String key) {
-		try {
+    @Override
+    public boolean remove(String key) {
+        try {
             cache.invalidate(key);
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Caffeine移除缓存出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Caffeine移除缓存出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	/**
-	 * 批量移除缓存
-	 */
-	public boolean removeAll(List<String> keys) {
-		try {
+    /**
+     * 批量移除缓存
+     */
+    @Override
+    public boolean remove(List<String> keys) {
+        try {
             cache.invalidateAll(keys);
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Caffeine批量移除缓存出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Caffeine批量移除缓存出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	/**
-	 * 清空所有缓存
-	 */
-	public boolean removeAll() {
-		try {
+    /**
+     * 清空所有缓存
+     */
+    @Override
+    public boolean removeAll() {
+        try {
             cache.invalidateAll();
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Caffeine清空所有缓存出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Caffeine清空所有缓存出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	/**
-	 * 获取缓存项数量
-	 */
-	public long size() {
-		long size = 0;
-		try {
-			size = cache.estimatedSize();
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-		} catch (Exception e) {
-			log.error("Caffeine获取缓存项数量出错，Error: {}", e.getMessage(), e);
-		}
-		return size;
-	}
+    /**
+     * 获取缓存项数量
+     */
+    @Override
+    public long size() {
+        long size = 0;
+        try {
+            size = cache.estimatedSize();
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+        } catch (Exception e) {
+            log.error("Caffeine获取缓存项数量出错，Error: {}", e.getMessage(), e);
+        }
+        return size;
+    }
 
-	/**
-	 * 获取所有缓存项的键
-	 */
-	public List<String> keys() {
-		List<String> list = new ArrayList<>();
-		try {
-			ConcurrentMap<String, String> map = cache.asMap();
-			for (Map.Entry<String, String> item : map.entrySet()) {
-				list.add(item.getKey());
-			}
-			if (log.isDebugEnabled()) {
-				log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-		} catch (Exception e) {
-			log.error("Caffeine获取所有缓存项的键出错，Error: {}", e.getMessage(), e);
-		}
-		return list;
-	}
+    /**
+     * 获取所有缓存项的键
+     */
+    @Override
+    public List<String> keys() {
+        List<String> list = new ArrayList<>();
+        try {
+            ConcurrentMap<String, String> map = cache.asMap();
+            for (Map.Entry<String, String> item : map.entrySet()) {
+                list.add(item.getKey());
+            }
+            if (log.isDebugEnabled()) {
+                log.debug("Caffeine缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+        } catch (Exception e) {
+            log.error("Caffeine获取所有缓存项的键出错，Error: {}", e.getMessage(), e);
+        }
+        return list;
+    }
 
-	/**
-	 * 缓存命中率
-	 */
-	public double getHitRate() {
-		return cache.stats().hitRate();
-	}
+    /**
+     * 缓存命中率
+     */
+    public double getHitRate() {
+        return cache.stats().hitRate();
+    }
 
-	/**
-	 * 加载新值的平均时间，单位为纳秒
-	 */
-	public double getAverageLoadPenalty() {
-		return cache.stats().averageLoadPenalty();
-	}
+    /**
+     * 加载新值的平均时间，单位为纳秒
+     */
+    public double getAverageLoadPenalty() {
+        return cache.stats().averageLoadPenalty();
+    }
 
-	/**
-	 * 缓存项被回收的总数，不包括显式清除
-	 */
-	public long getEvictionCount() {
-		return cache.stats().evictionCount();
-	}
+    /**
+     * 缓存项被回收的总数，不包括显式清除
+     */
+    public long getEvictionCount() {
+        return cache.stats().evictionCount();
+    }
 }
--- kuafu-data/src/main/java/com/yuweix/kuafu/data/cache/local/GuavaCache.java
+++ kuafu-data/src/main/java/com/yuweix/kuafu/data/cache/local/GuavaCache.java
@@ -16,14 +16,14 @@ import java.util.concurrent.TimeUnit;
  * @author yuwei
  */
 public class GuavaCache implements LocalCache {
-	private static final Logger log = LoggerFactory.getLogger(GuavaCache.class);
+    private static final Logger log = LoggerFactory.getLogger(GuavaCache.class);
 
 
-	private LoadingCache<String, String> cache = null;
+    private LoadingCache<String, String> cache = null;
 
 
     public GuavaCache(long duration) {
-		try {
+        try {
             cache = CacheBuilder.newBuilder()
                     .expireAfterAccess(duration, TimeUnit.SECONDS)
                     .removalListener(new RemovalListener<String, String>() {
@@ -42,168 +42,177 @@ public class GuavaCache implements LocalCache {
                             return null;
                         }
                     });
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * @param duration   缓存时间(单位：秒)
-	 */
-	public static GuavaCache create(long duration) {
-		return new GuavaCache(duration);
-	}
+    /**
+     * @param duration   缓存时间(单位：秒)
+     */
+    public static GuavaCache create(long duration) {
+        return new GuavaCache(duration);
+    }
 
-	public boolean put(String key, String value) {
-		try {
+    @Override
+    public boolean put(String key, String value) {
+        try {
             cache.put(key, value);
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Guava设置缓存值出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Guava设置缓存值出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	public boolean putAll(Map<String, String> map) {
-		try {
+    @Override
+    public boolean put(Map<String, String> map) {
+        try {
             cache.putAll(map);
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Guava批量设置缓存值出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Guava批量设置缓存值出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	public String get(String key) {
-		String val = null;
-		try {
-			val = cache.get(key);
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-		} catch (Exception e) {
-			log.error("Guava获取缓存值出错，Error: {}", e.getMessage(), e);
-		}
-		return val;
-	}
+    @Override
+    public String get(String key) {
+        String val = null;
+        try {
+            val = cache.get(key);
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+        } catch (Exception e) {
+            log.error("Guava获取缓存值出错，Error: {}", e.getMessage(), e);
+        }
+        return val;
+    }
 
-	public String getIfPresent(String key) {
-		String val = null;
-		try {
-			val = cache.getIfPresent(key);
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-		} catch (Exception e) {
-			log.error("Guava获取缓存值出错，Error: {}", e.getMessage(), e);
-		}
-		return val;
-	}
+    @Override
+    public String getIfPresent(String key) {
+        String val = null;
+        try {
+            val = cache.getIfPresent(key);
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+        } catch (Exception e) {
+            log.error("Guava获取缓存值出错，Error: {}", e.getMessage(), e);
+        }
+        return val;
+    }
 
-	public boolean remove(String key) {
-		try {
+    @Override
+    public boolean remove(String key) {
+        try {
             cache.invalidate(key);
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Guava移除缓存出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Guava移除缓存出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	/**
-	 * 批量移除缓存
-	 */
-	public boolean removeAll(List<String> keys) {
-		try {
+    /**
+     * 批量移除缓存
+     */
+    @Override
+    public boolean remove(List<String> keys) {
+        try {
             cache.invalidateAll(keys);
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Guava批量移除缓存出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Guava批量移除缓存出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	/**
-	 * 清空所有缓存
-	 */
-	public boolean removeAll() {
-		try {
+    /**
+     * 清空所有缓存
+     */
+    @Override
+    public boolean removeAll() {
+        try {
             cache.invalidateAll();
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-			return true;
-		} catch (Exception e) {
-			log.error("Guava清空所有缓存出错，Error: {}", e.getMessage(), e);
-			return false;
-		}
-	}
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("Guava清空所有缓存出错，Error: {}", e.getMessage(), e);
+            return false;
+        }
+    }
 
-	/**
-	 * 获取缓存项数量
-	 */
-	public long size() {
-		long size = 0;
-		try {
-			size = cache.size();
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-		} catch (Exception e) {
-			log.error("Guava获取缓存项数量出错，Error: {}", e.getMessage(), e);
-		}
-		return size;
-	}
+    /**
+     * 获取缓存项数量
+     */
+    @Override
+    public long size() {
+        long size = 0;
+        try {
+            size = cache.size();
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+        } catch (Exception e) {
+            log.error("Guava获取缓存项数量出错，Error: {}", e.getMessage(), e);
+        }
+        return size;
+    }
 
-	/**
-	 * 获取所有缓存项的键
-	 */
-	public List<String> keys() {
-		List<String> list = new ArrayList<>();
-		try {
-			ConcurrentMap<String, String> map = cache.asMap();
-			for (Map.Entry<String, String> item : map.entrySet()) {
-				list.add(item.getKey());
-			}
-			if (log.isDebugEnabled()) {
-				log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
-			}
-		} catch (Exception e) {
-			log.error("Guava获取所有缓存项的键出错，Error: {}", e.getMessage(), e);
-		}
-		return list;
-	}
+    /**
+     * 获取所有缓存项的键
+     */
+    @Override
+    public List<String> keys() {
+        List<String> list = new ArrayList<>();
+        try {
+            ConcurrentMap<String, String> map = cache.asMap();
+            for (Map.Entry<String, String> item : map.entrySet()) {
+                list.add(item.getKey());
+            }
+            if (log.isDebugEnabled()) {
+                log.debug("Guava缓存命中率：{}，新值平均加载时间：{}", getHitRate(), getAverageLoadPenalty());
+            }
+        } catch (Exception e) {
+            log.error("Guava获取所有缓存项的键出错，Error: {}", e.getMessage(), e);
+        }
+        return list;
+    }
 
-	/**
-	 * 缓存命中率
-	 */
-	public double getHitRate() {
-		return cache.stats().hitRate();
-	}
+    /**
+     * 缓存命中率
+     */
+    public double getHitRate() {
+        return cache.stats().hitRate();
+    }
 
-	/**
-	 * 加载新值的平均时间，单位为纳秒
-	 */
-	public double getAverageLoadPenalty() {
-		return cache.stats().averageLoadPenalty();
-	}
+    /**
+     * 加载新值的平均时间，单位为纳秒
+     */
+    public double getAverageLoadPenalty() {
+        return cache.stats().averageLoadPenalty();
+    }
 
-	/**
-	 * 缓存项被回收的总数，不包括显式清除
-	 */
-	public long getEvictionCount() {
-		return cache.stats().evictionCount();
-	}
+    /**
+     * 缓存项被回收的总数，不包括显式清除
+     */
+    public long getEvictionCount() {
+        return cache.stats().evictionCount();
+    }
 }
