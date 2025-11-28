5b0825496155660641b094e9ae0d36557f2bc8b8
wei.yu <weiy.yu@os.51job.com>
2025-11-28 14:41:44 +0800

    2.0.3

--- kuafu-data/src/main/java/com/yuweix/kuafu/data/datasecure/SensitiveUtil.java
+++ kuafu-data/src/main/java/com/yuweix/kuafu/data/datasecure/SensitiveUtil.java
@@ -13,25 +13,39 @@ import java.lang.reflect.Field;
 public class SensitiveUtil {
     private static final Logger log = LoggerFactory.getLogger(SensitiveUtil.class);
 
-    public static Object shield(Object object, String fieldName, Object val) {
-        if (!(val instanceof String) || "".equals(val)) {
-            return val;
-        }
+
+    public static Object shield(Object object, String fieldName) {
         try {
             Field field = object.getClass().getDeclaredField(fieldName);
+            Object val = field.get(object);
+            return shield(object, fieldName, val);
+        } catch (Exception ex) {
+            log.error("获取字段出错，Error: {}", ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    public static Object shield(Object object, String fieldName, Object val) {
+        try {
+            if (!(val instanceof String) || "".equals(val)) {
+                return val;
+            }
+            Field field = object.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
             Sensitive sensitive;
             if (String.class != field.getType() || (sensitive = field.getAnnotation(Sensitive.class)) == null) {
                 return val;
             }
-
-            return shield((String) val, sensitive.regex(), sensitive.replacement());
-        } catch (NoSuchFieldException ignored) {
-
+            String shieldVal = replace((String) val, sensitive.regex(), sensitive.replacement());
+            field.set(object, shieldVal);
+            return shieldVal;
+        } catch (Exception ex) {
+            log.error("获取字段出错，Error: {}", ex.getMessage(), ex);
+            return null;
         }
-        return val;
     }
 
-    public static String shield(String val, String regex, String replacement) {
+    private static String replace(String val, String regex, String replacement) {
         if (val == null || "".equals(val)
                 || regex == null || "".equals(regex.trim())
                 || replacement == null || "".equals(replacement.trim())) {
