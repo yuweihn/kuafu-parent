d7c773ec20d2758ee02f554652017f4bb89a5440
wei.yu <weiy.yu@os.51job.com>
2025-12-01 10:16:59 +0800

    2.0.3

--- kuafu-core/src/main/java/com/yuweix/kuafu/core/JacksonUtil.java
+++ kuafu-core/src/main/java/com/yuweix/kuafu/core/JacksonUtil.java
@@ -3,41 +3,99 @@ package com.yuweix.kuafu.core;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Type;
 
 
 /**
  * @author yuwei
  */
 public abstract class JacksonUtil {
-	private static ObjectMapper mapper = new ObjectMapper();
+    private static final Logger log = LoggerFactory.getLogger(JacksonUtil.class);
+    private static final ObjectMapper mapper = new ObjectMapper();
 
-	public static String toJSONString(Object obj) {
-		try {
-			return mapper.writeValueAsString(obj);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	public static Object parse(String text) {
-		try {
-			return mapper.readValue(text, Object.class);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	public static<T> T parseObject(String text, TypeReference<T> type) {
-		try {
-			return mapper.readValue(text, type);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	public static<T> T parseObject(String text, Class<T> clz) {
-		try {
-			return mapper.readValue(text, clz);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    static {
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+
+    public static String toString(Object obj) {
+        if (obj == null) {
+            log.error("Object cannot be null");
+            return null;
+        }
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException ex) {
+            log.error("Object cannot be processed. Error: {}", ex.getMessage());
+            return null;
+        }
+    }
+    public static Object toObject(String text) {
+        if (text == null || text.isEmpty()) {
+            log.error("JSON string cannot be null or empty");
+            return null;
+        }
+        try {
+            return mapper.readValue(text, Object.class);
+        } catch (JsonProcessingException ex) {
+            log.error("JSON string cannot be parsed. Error: {}", ex.getMessage());
+            return null;
+        }
+    }
+    public static<T> T toObject(String text, Type type) {
+        if (text == null || text.isEmpty()) {
+            log.error("JSON string cannot be null or empty");
+            return null;
+        }
+        if (type == null) {
+            log.error("Type cannot be null");
+            return null;
+        }
+        try {
+            JavaType javaType = TypeFactory.defaultInstance().constructType(type);
+            return mapper.readValue(text, javaType);
+        } catch (JsonProcessingException ex) {
+            log.error("JSON string cannot be parsed to type {}. Error: {}", type, ex.getMessage());
+            return null;
+        }
+    }
+    public static<T> T toObject(String text, TypeReference<T> type) {
+        if (text == null || text.isEmpty()) {
+            log.error("JSON string cannot be null or empty");
+            return null;
+        }
+        if (type == null) {
+            log.error("Type cannot be null");
+            return null;
+        }
+        try {
+            return mapper.readValue(text, type);
+        } catch (JsonProcessingException ex) {
+            log.error("JSON string cannot be parsed to type {}. Error: {}", type, ex.getMessage());
+            return null;
+        }
+    }
+    public static<T> T toObject(String text, Class<T> clz) {
+        if (text == null || text.isEmpty()) {
+            log.error("JSON string cannot be null or empty");
+            return null;
+        }
+        if (clz == null) {
+            log.error("Clz cannot be null");
+            return null;
+        }
+        try {
+            return mapper.readValue(text, clz);
+        } catch (JsonProcessingException ex) {
+            log.error("JSON string cannot be parsed to clz {}. Error: {}", clz, ex.getMessage());
+            return null;
+        }
+    }
 }
