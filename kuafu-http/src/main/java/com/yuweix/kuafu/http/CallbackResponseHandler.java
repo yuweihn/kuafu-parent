edc98a144fbe71c83f21dbe23ca442a028ae3bf9
wei.yu <weiy.yu@os.51job.com>
2025-12-02 19:29:34 +0800

    2.0.3

--- kuafu-http/src/main/java/com/yuweix/kuafu/http/CallbackResponseHandler.java
+++ kuafu-http/src/main/java/com/yuweix/kuafu/http/CallbackResponseHandler.java
@@ -273,7 +273,7 @@ public class CallbackResponseHandler<B> implements ResponseHandler<HttpResponse<
 
 		@Override
 		public String toString() {
-			return json.toString(this);
+            return json.toString(BasicHttpResponse.this);
 		}
 	}
 }
