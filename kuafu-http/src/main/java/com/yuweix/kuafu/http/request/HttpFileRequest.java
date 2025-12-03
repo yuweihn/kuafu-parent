21f9fa4e098cedce2212321ce3b07446f2215283
wei.yu <weiy.yu@os.51job.com>
2025-12-03 13:59:42 +0800

    1.0.4

--- kuafu-http/src/main/java/com/yuweix/kuafu/http/request/HttpFileRequest.java
+++ kuafu-http/src/main/java/com/yuweix/kuafu/http/request/HttpFileRequest.java
@@ -20,109 +20,109 @@ import com.yuweix.kuafu.http.response.HttpResponse;
  * 上传文件
  * @author yuwei
  */
-public class HttpFileRequest extends AbstractHttpRequest<HttpFileRequest> {
-	private List<FileField> fileFieldList;
-	private List<FormField> formFieldList;
+public class HttpFileRequest<B> extends AbstractHttpRequest<HttpFileRequest<B>, B> {
+    private List<FileField> fileFieldList;
+    private List<FormField> formFieldList;
 
 
-	private HttpFileRequest() {
-		super();
-		fileFieldList = new ArrayList<>();
-		formFieldList = new ArrayList<>();
-		method(HttpMethod.POST);
-	}
-	public static HttpFileRequest create() {
-		return new HttpFileRequest();
-	}
+    private HttpFileRequest() {
+        super();
+        fileFieldList = new ArrayList<>();
+        formFieldList = new ArrayList<>();
+        method(HttpMethod.POST);
+    }
+    public static <B>HttpFileRequest<B> create() {
+        return new HttpFileRequest<>();
+    }
 
 
-	public HttpFileRequest addFile(String fieldName, byte[] content, String fileName) {
-		FileWrapper fw = new FileWrapper();
-		fw.setContent(content);
-		fw.setFileName(fileName);
+    public HttpFileRequest<B> addFile(String fieldName, byte[] content, String fileName) {
+        FileWrapper fw = new FileWrapper();
+        fw.setContent(content);
+        fw.setFileName(fileName);
 
-		for (FileField ff: fileFieldList) {
-			if (fieldName.equals(ff.getFieldName())) {
-				ff.addFile(fw);
-				return this;
-			}
-		}
+        for (FileField ff: fileFieldList) {
+            if (fieldName.equals(ff.getFieldName())) {
+                ff.addFile(fw);
+                return this;
+            }
+        }
 
-		FileField ff = new FileField();
-		ff.setFieldName(fieldName);
-		ff.addFile(fw);
-		fileFieldList.add(ff);
-		return this;
-	}
+        FileField ff = new FileField();
+        ff.setFieldName(fieldName);
+        ff.addFile(fw);
+        fileFieldList.add(ff);
+        return this;
+    }
 
-	public HttpFileRequest formFieldList(Map<String, ?> map) {
-		if (map == null || map.isEmpty()) {
-			return this;
-		}
+    public HttpFileRequest<B> formFieldList(Map<String, ?> map) {
+        if (map == null || map.isEmpty()) {
+            return this;
+        }
 
-		List<FormField> formFieldList = new ArrayList<>();
-		for (Map.Entry<String, ?> entry: map.entrySet()) {
-			String key = entry.getKey();
-			Object value = entry.getValue();
-			if (key == null || value == null) {
-				continue;
-			}
-			formFieldList.add(new FormField(key, value));
-		}
-		return formFieldList(formFieldList);
-	}
-	public HttpFileRequest formFieldList(List<FormField> formFieldList) {
-		this.formFieldList.clear();
-		this.formFieldList.addAll(formFieldList);
-		return this;
-	}
-	public HttpFileRequest addFormField(String key, String value) {
-		if (key == null || "".equals(key) || value == null || "".equals(value)) {
-			return this;
-		}
+        List<FormField> formFieldList = new ArrayList<>();
+        for (Map.Entry<String, ?> entry: map.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (key == null || value == null) {
+                continue;
+            }
+            formFieldList.add(new FormField(key, value));
+        }
+        return formFieldList(formFieldList);
+    }
+    public HttpFileRequest<B> formFieldList(List<FormField> formFieldList) {
+        this.formFieldList.clear();
+        this.formFieldList.addAll(formFieldList);
+        return this;
+    }
+    public HttpFileRequest<B> addFormField(String key, String value) {
+        if (key == null || "".equals(key) || value == null || "".equals(value)) {
+            return this;
+        }
 
-		formFieldList.add(new FormField(key, value));
-		return this;
-	}
+        formFieldList.add(new FormField(key, value));
+        return this;
+    }
 
-	@Override
-	public <B>HttpResponse<B> execute() {
-		String charset = getCharset();
-		charset = charset != null ? charset : HttpConstant.ENCODING_UTF_8;
-		
-		ContentType textContentType = ContentType.create(ContentType.TEXT_PLAIN.getMimeType(), charset);
-		ContentType multipartContentType = ContentType.create(ContentType.MULTIPART_FORM_DATA.getMimeType(), charset);
+    @Override
+    public HttpResponse<B> execute() {
+        String charset = getCharset();
+        charset = charset != null ? charset : HttpConstant.ENCODING_UTF_8;
 
-		MultipartEntityBuilder builder = MultipartEntityBuilder.create()
-															.setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
-															.setCharset(Charset.forName(charset));
-		if (fileFieldList != null && fileFieldList.size() > 0) {
-			for (FileField fileField: fileFieldList) {
-				List<FileWrapper> fileList = fileField.getFileList();
-				if (fileList != null && fileList.size()> 0) {
-					for (FileWrapper fw: fileList) {
-						builder.addBinaryBody(fileField.getFieldName(), fw.getContent(), multipartContentType, fw.getFileName());
-					}
-				}
-			}
-		}
+        ContentType textContentType = ContentType.create(ContentType.TEXT_PLAIN.getMimeType(), charset);
+        ContentType multipartContentType = ContentType.create(ContentType.MULTIPART_FORM_DATA.getMimeType(), charset);
 
-		if (formFieldList != null && formFieldList.size() > 0) {
-			for (FormField ff: formFieldList) {
-				String k = ff.getKey();
-				String v = ff.getValue();
-				if (k == null || "".equals(k) || v == null || "".equals(v)) {
-					continue;
-				}
-				builder.addTextBody(k, v, textContentType);
-			}
-		}
-		HttpEntity entity = builder.build();
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create()
+                .setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
+                .setCharset(Charset.forName(charset));
+        if (fileFieldList != null && fileFieldList.size() > 0) {
+            for (FileField fileField: fileFieldList) {
+                List<FileWrapper> fileList = fileField.getFileList();
+                if (fileList != null && fileList.size()> 0) {
+                    for (FileWrapper fw: fileList) {
+                        builder.addBinaryBody(fileField.getFieldName(), fw.getContent(), multipartContentType, fw.getFileName());
+                    }
+                }
+            }
+        }
 
-		HttpEntityEnclosingRequestBase requestBase = getRequestBase();
-		requestBase.addHeader("Content-Disposition", "attachment");
-		requestBase.setEntity(entity);
-		this.setHttpUriRequest(requestBase);
-		return execute0();
-	}
+        if (formFieldList != null && formFieldList.size() > 0) {
+            for (FormField ff: formFieldList) {
+                String k = ff.getKey();
+                String v = ff.getValue();
+                if (k == null || "".equals(k) || v == null || "".equals(v)) {
+                    continue;
+                }
+                builder.addTextBody(k, v, textContentType);
+            }
+        }
+        HttpEntity entity = builder.build();
+
+        HttpEntityEnclosingRequestBase requestBase = getRequestBase();
+        requestBase.addHeader("Content-Disposition", "attachment");
+        requestBase.setEntity(entity);
+        this.setHttpUriRequest(requestBase);
+        return execute0();
+    }
 }
