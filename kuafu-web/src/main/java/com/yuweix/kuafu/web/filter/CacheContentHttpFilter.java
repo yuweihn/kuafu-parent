8ea4fb9f682a4497fee553809e33218764a9db5c
wei.yu <wei.yu@51job.com>
2026-03-26 15:17:32 +0800

    1.0.5

--- kuafu-web/src/main/java/com/yuweix/kuafu/web/filter/CacheContentHttpFilter.java
+++ kuafu-web/src/main/java/com/yuweix/kuafu/web/filter/CacheContentHttpFilter.java
@@ -1,13 +1,11 @@
 package com.yuweix.kuafu.web.filter;
 
 
-import com.yuweix.kuafu.core.JsonUtil;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -17,18 +15,6 @@ import java.util.Map;
  * @author yuwei
  */
 public class CacheContentHttpFilter extends AbstractFilter<CacheBodyRequestWrapper, ContentCachingResponseWrapper> {
-	private Integer maxRequestSize = null;
-	private Integer maxResponseSize = null;
-
-
-    public void setMaxRequestSize(Integer maxRequestSize) {
-        this.maxRequestSize = maxRequestSize;
-    }
-	public void setMaxResponseSize(Integer maxResponseSize) {
-		this.maxResponseSize = maxResponseSize;
-	}
-
-
 	@Override
 	protected CacheBodyRequestWrapper wrap(HttpServletRequest request) {
 		return new CacheBodyRequestWrapper(request);
@@ -55,27 +41,10 @@ public class CacheContentHttpFilter extends AbstractFilter<CacheBodyRequestWrapp
 		return logInfoMap;
 	}
 
-	private Object getRequestBody(CacheBodyRequestWrapper request) {
-		byte[] bytes = request.getRequestBody();
-		if (bytes.length <= 0) {
-			return null;
-		}
-
-		String content = new String(bytes, Charset.forName(getEncoding()));
-		if ("".equals(content)) {
-			return null;
-		}
-		try {
-			content = URLDecoder.decode(content, getEncoding());
-		} catch (Exception ignored) {
-		}
-		return limit(content, maxRequestSize);
-	}
-
 	@Override
 	protected Object getResponseBody(ContentCachingResponseWrapper response) {
 		String str = new String(response.getContentAsByteArray(), Charset.forName(getEncoding()));
-		return limit(str, maxResponseSize);
+		return limit(str, getMaxResponseSize());
 	}
 
 	protected void writeResponseBody(ContentCachingResponseWrapper response) {
