3d8a9d187b0a9acb90d9bed2a7a1eb07cc2dcc90
yuwei <yuwei@yuweix.com>
2026-06-09 22:41:12 +0800

    2.0.5

--- kuafu-core/src/main/java/com/yuweix/kuafu/core/serialize/JacksonUtil.java
+++ kuafu-core/src/main/java/com/yuweix/kuafu/core/serialize/JacksonUtil.java
@@ -29,24 +29,65 @@ import java.util.TimeZone;
 public abstract class JacksonUtil {
     private static final Logger log = LoggerFactory.getLogger(JacksonUtil.class);
 
-    private static final ObjectMapper mapper = new ObjectMapper();
+    /**
+     * 默认的ObjectMapper
+     */
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    /**
+     * 不脱敏、保留类型信息
+     */
+    private static final ObjectMapper MAPPER2 = new ObjectMapper();
+    /**
+     * 不脱敏、不保留类型信息
+     */
+    private static final ObjectMapper MAPPER3 = new ObjectMapper();
     static {
-        mapper.registerModule(new JavaTimeModule());
-        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-        mapper.setTimeZone(TimeZone.getTimeZone(Constant.DEFAULT_TIME_ZONE));
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        initMapper();
+        initMapper2();
+        initMapper3();
+    }
 
+    private static void initMapper() {
+        MAPPER.registerModule(new JavaTimeModule());
+        MAPPER.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        MAPPER.setTimeZone(TimeZone.getTimeZone(Constant.DEFAULT_TIME_ZONE));
+        MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        /**
+         * 脱敏配置
+         */
         JacksonSensitiveFilter jacksonSensitiveFilter = new JacksonSensitiveFilter();
         SimpleModule jacksonSensitiveFilterModule = new SimpleModule(jacksonSensitiveFilter.getClass().getName());
         jacksonSensitiveFilterModule.setSerializerModifier(jacksonSensitiveFilter);
-        mapper.registerModule(jacksonSensitiveFilterModule);
+        MAPPER.registerModule(jacksonSensitiveFilterModule);
 
         // 配置类型验证器以支持安全的多态类型处理
         PolymorphicTypeValidator typeValidator = BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build();
         // 启用默认类型信息，用于序列化和反序列化时保留类型信息
-        mapper.activateDefaultTyping(typeValidator, ObjectMapper.DefaultTyping.NON_FINAL);
+        MAPPER.activateDefaultTyping(typeValidator, ObjectMapper.DefaultTyping.NON_FINAL);
+    }
+    private static void initMapper2() {
+        MAPPER2.registerModule(new JavaTimeModule());
+        MAPPER2.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        MAPPER2.setTimeZone(TimeZone.getTimeZone(Constant.DEFAULT_TIME_ZONE));
+        MAPPER2.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        MAPPER2.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        MAPPER2.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        // 配置类型验证器以支持安全的多态类型处理
+        PolymorphicTypeValidator typeValidator = BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build();
+        // 启用默认类型信息，用于序列化和反序列化时保留类型信息
+        MAPPER2.activateDefaultTyping(typeValidator, ObjectMapper.DefaultTyping.NON_FINAL);
+    }
+    private static void initMapper3() {
+        MAPPER3.registerModule(new JavaTimeModule());
+        MAPPER3.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        MAPPER3.setTimeZone(TimeZone.getTimeZone(Constant.DEFAULT_TIME_ZONE));
+        MAPPER3.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        MAPPER3.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        MAPPER3.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
 
@@ -56,7 +97,7 @@ public abstract class JacksonUtil {
             return null;
         }
         try {
-            return mapper.writeValueAsString(obj);
+            return MAPPER.writeValueAsString(obj);
         } catch (JsonProcessingException ex) {
             log.error("Object cannot be processed. Error: {}", ex.getMessage(), ex);
             return null;
@@ -67,7 +108,7 @@ public abstract class JacksonUtil {
             return null;
         }
         try {
-            return mapper.readValue(text, new TypeReference<T>() {});
+            return MAPPER.readValue(text, new TypeReference<T>() {});
         } catch (Exception ex) {
             log.error("JSON string cannot be parsed. Error: {}", ex.getMessage(), ex);
             return null;
@@ -84,7 +125,7 @@ public abstract class JacksonUtil {
         }
         try {
             JavaType javaType = TypeFactory.defaultInstance().constructType(type);
-            return mapper.readValue(text, javaType);
+            return MAPPER.readValue(text, javaType);
         } catch (JsonProcessingException ex) {
             log.error("JSON string cannot be parsed to type {}. Error: {}", type, ex.getMessage(), ex);
             return null;
@@ -100,7 +141,7 @@ public abstract class JacksonUtil {
             return null;
         }
         try {
-            return mapper.readValue(text, type);
+            return MAPPER.readValue(text, type);
         } catch (JsonProcessingException ex) {
             log.error("JSON string cannot be parsed to type {}. Error: {}", type, ex.getMessage(), ex);
             return null;
@@ -116,7 +157,7 @@ public abstract class JacksonUtil {
             return null;
         }
         try {
-            return mapper.readValue(text, clz);
+            return MAPPER.readValue(text, clz);
         } catch (JsonProcessingException ex) {
             log.error("JSON string cannot be parsed to clz {}. Error: {}", clz, ex.getMessage(), ex);
             return null;
--- kuafu-core/src/main/java/com/yuweix/kuafu/core/springboot/JacksonConf.java
+++ kuafu-core/src/main/java/com/yuweix/kuafu/core/springboot/JacksonConf.java
@@ -36,8 +36,9 @@ public class JacksonConf {
      */
     @ConditionalOnMissingBean(ObjectMapper.class)
     @Bean
-    public ObjectMapper objectMapper(@Value("${kuafu.json.jackson.time-zone:" + Constant.DEFAULT_TIME_ZONE + "}") String timeZone
-            , @Value("${kuafu.json.jackson.serializer-modifiers:}") String serializerModifiers) {
+    public ObjectMapper objectMapper(@Value("${kuafu.json.jackson.mapper.time-zone:" + Constant.DEFAULT_TIME_ZONE + "}") String timeZone
+            , @Value("${kuafu.json.jackson.mapper.sensitive:true}") boolean sensitive
+            , @Value("${kuafu.json.jackson.mapper.serializer-modifiers:}") String serializerModifiers) {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
@@ -46,10 +47,15 @@ public class JacksonConf {
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        JacksonSensitiveFilter jacksonSensitiveFilter = new JacksonSensitiveFilter();
-        SimpleModule jacksonSensitiveFilterModule = new SimpleModule(jacksonSensitiveFilter.getClass().getName());
-        jacksonSensitiveFilterModule.setSerializerModifier(jacksonSensitiveFilter);
-        mapper.registerModule(jacksonSensitiveFilterModule);
+        /**
+         * 脱敏配置
+         */
+        if (sensitive) {
+            JacksonSensitiveFilter jacksonSensitiveFilter = new JacksonSensitiveFilter();
+            SimpleModule jacksonSensitiveFilterModule = new SimpleModule(jacksonSensitiveFilter.getClass().getName());
+            jacksonSensitiveFilterModule.setSerializerModifier(jacksonSensitiveFilter);
+            mapper.registerModule(jacksonSensitiveFilterModule);
+        }
 
         if (serializerModifiers != null && !"".equals(serializerModifiers.trim())) {
             List<BeanSerializerModifier> modifiers = new ArrayList<>();
@@ -62,7 +68,7 @@ public class JacksonConf {
                     mod = mod.trim();
                     try {
                         Class<?> clz = Class.forName(mod);
-                        BeanSerializerModifier modifier = (BeanSerializerModifier) clz.getDeclaredConstructor().newInstance();
+                        BeanSerializerModifier modifier = (BeanSerializerModifier) clz.newInstance();
                         modifiers.add(modifier);
                     } catch (Exception ex) {
                         log.error("BeanSerializerModifier cannot be created. Error: {}", ex.getMessage(), ex);
