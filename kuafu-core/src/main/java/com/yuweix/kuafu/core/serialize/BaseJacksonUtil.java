66b4bba6335c5df54b67bb09e55b16d4aecbb23d
yuwei <yuwei@yuweix.com>
2026-06-09 22:56:51 +0800

    2.0.5

--- kuafu-core/src/main/java/com/yuweix/kuafu/core/serialize/BaseJacksonUtil.java
+++ kuafu-core/src/main/java/com/yuweix/kuafu/core/serialize/BaseJacksonUtil.java
@@ -8,13 +8,9 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.yuweix.kuafu.core.Constant;
-import com.yuweix.kuafu.core.serialize.jackson.JacksonSensitiveFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,37 +22,24 @@ import java.util.TimeZone;
 /**
  * @author yuwei
  */
-public abstract class JacksonUtil {
-    private static final Logger log = LoggerFactory.getLogger(JacksonUtil.class);
+public abstract class BaseJacksonUtil {
+    private static final Logger log = LoggerFactory.getLogger(BaseJacksonUtil.class);
 
     /**
-     * 脱敏、保留类型信息
+     * 不脱敏、不保留类型信息
      */
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final ObjectMapper BASE_MAPPER = new ObjectMapper();
     static {
-        initMapper();
+        initBaseMapper();
     }
 
-    private static void initMapper() {
-        MAPPER.registerModule(new JavaTimeModule());
-        MAPPER.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-        MAPPER.setTimeZone(TimeZone.getTimeZone(Constant.DEFAULT_TIME_ZONE));
-        MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        /**
-         * 脱敏配置
-         */
-        JacksonSensitiveFilter jacksonSensitiveFilter = new JacksonSensitiveFilter();
-        SimpleModule jacksonSensitiveFilterModule = new SimpleModule(jacksonSensitiveFilter.getClass().getName());
-        jacksonSensitiveFilterModule.setSerializerModifier(jacksonSensitiveFilter);
-        MAPPER.registerModule(jacksonSensitiveFilterModule);
-
-        // 配置类型验证器以支持安全的多态类型处理
-        PolymorphicTypeValidator typeValidator = BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build();
-        // 启用默认类型信息，用于序列化和反序列化时保留类型信息
-        MAPPER.activateDefaultTyping(typeValidator, ObjectMapper.DefaultTyping.NON_FINAL);
+    private static void initBaseMapper() {
+        BASE_MAPPER.registerModule(new JavaTimeModule());
+        BASE_MAPPER.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        BASE_MAPPER.setTimeZone(TimeZone.getTimeZone(Constant.DEFAULT_TIME_ZONE));
+        BASE_MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        BASE_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        BASE_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
 
@@ -66,7 +49,7 @@ public abstract class JacksonUtil {
             return null;
         }
         try {
-            return MAPPER.writeValueAsString(obj);
+            return BASE_MAPPER.writeValueAsString(obj);
         } catch (JsonProcessingException ex) {
             log.error("Object cannot be processed. Error: {}", ex.getMessage(), ex);
             return null;
@@ -77,7 +60,7 @@ public abstract class JacksonUtil {
             return null;
         }
         try {
-            return MAPPER.readValue(text, new TypeReference<T>() {});
+            return BASE_MAPPER.readValue(text, new TypeReference<T>() {});
         } catch (Exception ex) {
             log.error("JSON string cannot be parsed. Error: {}", ex.getMessage(), ex);
             return null;
@@ -94,7 +77,7 @@ public abstract class JacksonUtil {
         }
         try {
             JavaType javaType = TypeFactory.defaultInstance().constructType(type);
-            return MAPPER.readValue(text, javaType);
+            return BASE_MAPPER.readValue(text, javaType);
         } catch (JsonProcessingException ex) {
             log.error("JSON string cannot be parsed to type {}. Error: {}", type, ex.getMessage(), ex);
             return null;
@@ -110,7 +93,7 @@ public abstract class JacksonUtil {
             return null;
         }
         try {
-            return MAPPER.readValue(text, type);
+            return BASE_MAPPER.readValue(text, type);
         } catch (JsonProcessingException ex) {
             log.error("JSON string cannot be parsed to type {}. Error: {}", type, ex.getMessage(), ex);
             return null;
@@ -126,7 +109,7 @@ public abstract class JacksonUtil {
             return null;
         }
         try {
-            return MAPPER.readValue(text, clz);
+            return BASE_MAPPER.readValue(text, clz);
         } catch (JsonProcessingException ex) {
             log.error("JSON string cannot be parsed to clz {}. Error: {}", clz, ex.getMessage(), ex);
             return null;
