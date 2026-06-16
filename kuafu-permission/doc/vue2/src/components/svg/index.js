4ebd0e427b9b646ed4c806fcb6000c53f67c09f5
wei.yu <wei.yu@51job.com>
2026-06-16 14:21:30 +0800

    2.0.5

--- kuafu-permission/doc/vue2/src/components/svg/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import SvgIcon from './index.vue'// svg component
-
-// register globally
-Vue.component('svg-icon', SvgIcon)
-
-const req = require.context('./icon', false, /\.svg$/)
-const requireAll = requireContext => requireContext.keys().map(requireContext)
-requireAll(req)
