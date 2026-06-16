e0d6ba2f90f482b6c43ceef1489cc782be1c1a78
wei.yu <wei.yu@51job.com>
2026-06-16 16:44:41 +0800

    2.0.5

--- kuafu-permission/doc/vue2/src/vuex/store.js
+++ kuafu-permission/doc/vue2/src/vuex/store.js
@@ -7,18 +7,18 @@ import dynamicButtons from '@/dynamic.buttons';
 Vue.use(Vuex);
 
 const getters = {
-  routes: state0 => state0.dynamicRoutes.routes,
-  isDynamicMenuLoaded: state0 => state0.dynamicRoutes.isDynamicMenuLoaded,
-  buttons: state0 => state0.dynamicButtons.buttons,
-  isDynamicButtonLoaded: state0 => state0.dynamicButtons.isDynamicButtonLoaded
+    routes: state0 => state0.dynamicRoutes.routes,
+    isDynamicMenuLoaded: state0 => state0.dynamicRoutes.isDynamicMenuLoaded,
+    buttons: state0 => state0.dynamicButtons.buttons,
+    isDynamicButtonLoaded: state0 => state0.dynamicButtons.isDynamicButtonLoaded
 }
 
 const store = new Vuex.Store({
-  modules: {
-    dynamicRoutes,
-    dynamicButtons
-  },
-  getters
+    modules: {
+        dynamicRoutes,
+        dynamicButtons
+    },
+    getters
 });
 
 export default store;
