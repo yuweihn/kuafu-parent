25a8a6358604664cfdfb2e023fcb89696fc3fe1e
yuwei <yuwei@yuweix.com>
2026-06-19 11:14:39 +0800

    2.0.5

--- kuafu-permission/doc/vue2/src/views/admin/components/EditAdmin.vue
+++ kuafu-permission/doc/vue2/src/views/admin/components/EditAdmin.vue
@@ -8,7 +8,7 @@
 				<el-input v-model="editForm.realName" clearable autocomplete="off" style="width: 100%"/>
 			</el-form-item>
 			<el-form-item label="性别" prop="gender">
-				<el-select v-model="editForm.gender" placeholder="请选择" clearable filterable style="width: 100px">
+				<el-select v-model="editForm.gender" placeholder="请选择" clearable filterable style="width: 150px">
 					<el-option v-for="item in genderOptions" :key="item.val" :label="item.label" :value="item.val" />
 				</el-select>
 			</el-form-item>
