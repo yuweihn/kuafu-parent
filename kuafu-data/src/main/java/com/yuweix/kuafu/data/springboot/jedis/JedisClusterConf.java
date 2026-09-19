97d9cdc17a45cd5d7cb84a7460fff82f57edc5bd
yuwei <yuwei@yuweix.com>
2026-09-19 13:17:50 +0800

    2.0.6

--- kuafu-data/src/main/java/com/yuweix/kuafu/data/springboot/jedis/JedisClusterConf.java
+++ kuafu-data/src/main/java/com/yuweix/kuafu/data/springboot/jedis/JedisClusterConf.java
@@ -27,13 +27,17 @@ public class JedisClusterConf {
 			, @Value("${kuafu.redis.pool.max-idle:10}") int maxIdle
 			, @Value("${kuafu.redis.pool.min-idle:10}") int minIdle
 			, @Value("${kuafu.redis.pool.max-wait-millis:10000}") long maxWaitMillis
-			, @Value("${kuafu.redis.pool.test-on-borrow:false}") boolean testOnBorrow) {
+			, @Value("${kuafu.redis.pool.time-between-eviction-runs-millis:30000}") long timeBetweenEvictionRunsMillis
+			, @Value("${kuafu.redis.pool.test-on-borrow:false}") boolean testOnBorrow
+			, @Value("${kuafu.redis.pool.test-while-idle:true}") boolean testWhileIdle) {
 		JedisPoolConfig config = new JedisPoolConfig();
 		config.setMaxTotal(maxTotal);
 		config.setMaxIdle(maxIdle);
 		config.setMinIdle(minIdle);
 		config.setMaxWaitMillis(maxWaitMillis);
+		config.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
 		config.setTestOnBorrow(testOnBorrow);
+		config.setTestWhileIdle(testWhileIdle);
 		return config;
 	}
 
